d62c2367c4f7da0e5e7a5f89c581c362ec911dd2
lindaya0913 <lindy0913@gmail.com>
2020-12-06 18:23:00 +0800

    create pokezon-api project

---
+++ app/presentation/representers/pokemon_representers.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: false
+
+require_relative 'ability'
+require_relative 'evochain'
+require_relative 'type'
+
+module MerciDanke
+  module Entity
+    # Domain entity for pokemons
+    class Pokemon < Dry::Struct
+      include Dry.Types
+
+      attribute :id, Integer.optional
+      attribute :origin_id, Strict::Integer
+      attribute :poke_name, Strict::String
+      attribute :types, Strict::Array.of(Type)
+      attribute :height, Strict::Integer
+      attribute :weight, Strict::Integer
+      attribute :back_default, Strict::String
+      attribute :back_shiny, Strict::String
+      attribute :front_default, Strict::String
+      attribute :front_shiny, Strict::String
+      attribute :habitat, Strict::String
+      attribute :color, Strict::String
+      attribute :flavor_text_entries, Strict::String
+      attribute :genera, Strict::String
+      # Evochain
+      attribute :evochain, Evochain
+      # Ability
+      attribute :abilities, Strict::Array.of(Ability)
+
+      def to_attr_hash
+        to_hash.reject { |key, _| %i[id evochain abilities types].include? key }
+      end
+    end
+  end
+end
---
+++ app/presentation/representers/product_representers.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: false
+
+module MerciDanke
+  module Entity
+    # Domain entity for any coding projects
+    class Product < Dry::Struct
+      include Dry.Types
+
+      attribute :id,        Integer.optional
+      attribute :origin_id, Strict::String
+      attribute :poke_name, Strict::String
+      attribute :title,     Strict::String
+      attribute :link,      Strict::String
+      attribute :image,     Strict::String
+      attribute :rating,    Strict::Float
+      attribute :ratings_total, Strict::Integer
+      # attribute :price, Strict::Float
+      # attribute :currency, Strict::String
+
+      def to_attr_hash
+        to_hash.reject { |key, _| [:id].include? key }
+      end
+    end
+  end
+end
