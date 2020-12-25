e5dc4c1392e87843f36bbbade121dd15efba9cde
chinyakao <chinyariley@gmail.com>
2020-12-25 15:54:08 +0800

    delete puts in workers

--- workers/search_products_worker.rb
+++ workers/search_products_worker.rb
@@ -21,16 +21,8 @@ class SearchProductsWorker
   shoryuken_options queue: config.CLONE_QUEUE_URL, auto_delete: true
 
   def perform(_sqs_msg, request)
-    # puts '_sqs_msg: ', _sqs_msg
-    puts 'request: ', request
-    puts 'request class: ', request.class
     am_products = MerciDanke::Amazon::ProductMapper.new.find(request, MerciDanke::App.config.API_KEY)
-    puts 'am_products1: ', am_products
     am_products.map { |prod| MerciDanke::SearchRecord::For.entity(prod).create(prod) }
-    puts 'am_products2: ', am_products
-    # project = CodePraise::Representer::Project
-    #   .new(OpenStruct.new).from_json(request)
-    # CodePraise::GitRepo.new(project).clone!
   rescue StandardError
     puts 'AMAZON is busy. Please try again!'
   end
