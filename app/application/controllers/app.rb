3e507481c0d8c91deb3bce3556f777ffc031f104
Shelly Chao <shelly.chao@iss.nthu.edu.tw>
2021-01-12 20:46:33 +0800

    change pokemon total num

--- app/application/controllers/app.rb
+++ app/application/controllers/app.rb
@@ -16,8 +16,8 @@ module MerciDanke
 
       # GET /
       routing.root do
-        25.times do |num|
-          break if Database::PokemonOrm.find(id: 25)
+        400.times do |num|
+          break if Database::PokemonOrm.find(id: 400)
 
           pokemons = Pokemon::PokemonMapper.new.find((num + 1).to_s)
           SearchRecord::ForPoke.entity(pokemons).create(pokemons)
